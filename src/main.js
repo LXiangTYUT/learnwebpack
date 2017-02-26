45315ad12aaa1662143b1ce96205662902e94dc4
LXiangTYUT <lxiang_ty@icloud.com>
2017-02-26 20:01:37 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+/**
+ * Created by liuxiang_ty on 2017/2/26.
+ */
+document.write("<h1>Hello ,world!</h1>")
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by liuxiang_ty on 2017/2/26.
+ */
+
+module.exports = {
+    entry:"./src/main.js",
+    output:{
+        path: __dirname + '/build/',
+        filename:'[name].bundle.js'
+    }
+    
+};
